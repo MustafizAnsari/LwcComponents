3feda75f389e5af72683ac1261ab4a61e7cbc3bb
MustafizAnsari <mustafiz.ansari@gmail.com>
2023-09-12 18:22:52 +0530

    Recent opportunity componnt based on amount

--- force-app/main/default/lwc/opportunityDataTable/opportunityDataTable.js
+++ force-app/main/default/lwc/opportunityDataTable/opportunityDataTable.js
@@ -18,6 +18,7 @@ export default class OpportunityDataTable extends LightningElement
           this.opportunities = data;
 
     }else if(error){
+        
         console.error(error);
     }
    }
